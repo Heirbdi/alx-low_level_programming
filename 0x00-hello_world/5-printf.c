a6d5b4933b607b9bd2e46ce82ba6c71aac469a73
Heirbdi <heritierzlatan2@gmail.com>
2022-06-03 08:04:20 -0700

    Printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
-
-int main (void) {
-	puts("\"programming is like building a multilingual puzzle");
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
