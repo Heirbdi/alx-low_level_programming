193dce00ecdad281d2b152c43223b9c377c67d65
Heirbdi <heritierzlatan2@gmail.com>
2022-06-03 14:18:46 -0700

    Positive or Negative Number

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -13,7 +13,7 @@ int main(void)
 	int n;
 
 	srand(time(0));
-	n = rand() - RAND_max / 2;
+	n = rand() - RAND_MAX / 2;
 	if (n < 0)
 	{
 		printf("%d is negative\n", n);
@@ -26,3 +26,5 @@ int main(void)
 	}
 	return (0);
 }
+
+
