40dc791d370d7e047edd299d484a679c6236937d
root <heritierzlatan2@gmail.com>
2022-06-10 09:33:04 -0700

    Check for uppercase character

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,15 +1,26 @@
-#include "main.h"
-#include <stdio.h>
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int __isupper(int c);
-{
-	if (c >= 'A' && c <= 'Z')
-	{
-		return (1);
-	}
-	return (0);
-}
+On branch master
+Your branch is up to date with 'origin/master'.
+
+Changes not staged for commit:
+  (use "git add <file>..." to update what will be committed)
+  (use "git restore <file>..." to discard changes in working directory)
+	modified:   ../0x00-hello_world/0-preprocessor
+	modified:   ../0x02-functions_nested_loops/1-alphabet.c
+	modified:   ../0x02-functions_nested_loops/1-main.c
+	modified:   ../0x02-functions_nested_loops/2-main.c
+	modified:   ../0x02-functions_nested_loops/2-print_alphabet_x10.c
+	modified:   ../0x02-functions_nested_loops/3-islower.c
+	modified:   ../0x02-functions_nested_loops/4-isalpha.c
+	modified:   ../0x02-functions_nested_loops/4-main.c
+	modified:   ../0x02-functions_nested_loops/5-sign.c
+	modified:   ../0x02-functions_nested_loops/6-abs.c
+	modified:   ../0x02-functions_nested_loops/7-print_last_digit.c
+	modified:   ../0x02-functions_nested_loops/main.h
+	modified:   0-isupper.c
+	modified:   0-main.c
+
+Untracked files:
+  (use "git add <file>..." to include in what will be committed)
+	../0x02-functions_nested_loops/.1-alphabet.c.swp
+
+no changes added to commit (use "git add" and/or "git commit -a")
