2a03e1116e5dc79639f665fc356499845202cb64
root <heritierzlatan2@gmail.com>
2022-06-10 09:38:47 -0700

    Checks for uppercase

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,26 +1,11 @@
-On branch master
-Your branch is up to date with 'origin/master'.
-
-Changes not staged for commit:
-  (use "git add <file>..." to update what will be committed)
-  (use "git restore <file>..." to discard changes in working directory)
-	modified:   ../0x00-hello_world/0-preprocessor
-	modified:   ../0x02-functions_nested_loops/1-alphabet.c
-	modified:   ../0x02-functions_nested_loops/1-main.c
-	modified:   ../0x02-functions_nested_loops/2-main.c
-	modified:   ../0x02-functions_nested_loops/2-print_alphabet_x10.c
-	modified:   ../0x02-functions_nested_loops/3-islower.c
-	modified:   ../0x02-functions_nested_loops/4-isalpha.c
-	modified:   ../0x02-functions_nested_loops/4-main.c
-	modified:   ../0x02-functions_nested_loops/5-sign.c
-	modified:   ../0x02-functions_nested_loops/6-abs.c
-	modified:   ../0x02-functions_nested_loops/7-print_last_digit.c
-	modified:   ../0x02-functions_nested_loops/main.h
-	modified:   0-isupper.c
-	modified:   0-main.c
-
-Untracked files:
-  (use "git add <file>..." to include in what will be committed)
-	../0x02-functions_nested_loops/.1-alphabet.c.swp
-
-no changes added to commit (use "git add" and/or "git commit -a")
+#include "main.h"
+/**
+ * _isupper - checks for upper case character
+ * @c: the character to check for
+ * Description: checks for upper character.
+ * Return: 1 if c is upper case. 0 otherwise
+ */
+int _isupper(int c)
+{
+	return (c >= 'A' && c <= 'Z');
+}
