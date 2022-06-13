01b8b5aae2c5a8221bed3751316555287f6a73f6
Heirbdi <heritierzlatan2@gmail.com>
2022-06-13 14:37:54 -0700

    Print all digit number

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,3 +1,14 @@
-[master e181876] 0 to 10
- 1 file changed, 16 insertions(+)
- create mode 100755 0x01-variables_if_else_while/5-print_numbers.c
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0
+ */
+int main(void)
+{
+	int digit;
+
+	for (digit = 0; digit < 10; digit++)
+		printf("%i", digit);
+	putchar('\n');
+	return (0);
+}
