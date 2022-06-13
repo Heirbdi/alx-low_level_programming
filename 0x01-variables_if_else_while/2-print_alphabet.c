0cdc286cb7e3bf820cf667f3723641717a9cc598
Heirbdi <heritierzlatan2@gmail.com>
2022-06-13 14:22:38 -0700

    Alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,4 +1,20 @@
-[master efe6c1f] A to Z
- 2 files changed, 19 insertions(+), 32 deletions(-)
- rewrite 0x01-variables_if_else_while/1-last_digit.c (100%)
- create mode 100755 0x01-variables_if_else_while/2-print_alphabet.c
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - check for conditions
+ * Description: return the results for the condition
+ * Return: return the integer value 0
+ */
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
