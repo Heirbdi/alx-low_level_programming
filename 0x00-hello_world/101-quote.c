c665b132c795fd24c88918b0221bbcd1256853db
Heirbdi <heritierzlatan2@gmail.com>
2022-06-13 14:01:01 -0700

    Print exactly

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,22 +1,11 @@
-On branch master
-Your branch is up to date with 'origin/master'.
-
-Changes not staged for commit:
-  (use "git add/rm <file>..." to update what will be committed)
-  (use "git restore <file>..." to discard changes in working directory)
-	deleted:    .3-name.swp
-	deleted:    .5-printf.c.swp
-	modified:   1-compiler
-	modified:   100-intel
-	modified:   101-quote.c
-	modified:   2-assembler
-	modified:   3-name
-	modified:   4-puts.c
-	modified:   5-printf.c
-	modified:   6-size.c
-	modified:   ../0x05-pointers_arrays_strings/100-atoi.c
-	modified:   ../0x05-pointers_arrays_strings/101-keygen.c
-	modified:   ../0x05-pointers_arrays_strings/8-print_array.c
-	modified:   ../0x05-pointers_arrays_strings/9-strcpy.c
-
-no changes added to commit (use "git add" and/or "git commit -a")
+#include <unistd.h>
+/**
+ * main - Entry point
+ * Description: "prints exactly as is"
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
