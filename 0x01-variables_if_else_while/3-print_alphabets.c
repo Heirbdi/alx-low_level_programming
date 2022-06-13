990c1f20479ba498ca8c7f1c1d5d865f98ae061c
Heirbdi <heritierzlatan2@gmail.com>
2022-06-13 14:26:33 -0700

    Alphabet in lowercase

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,3 +1,16 @@
-[master 43acf8f] A to Z
- 1 file changed, 18 insertions(+)
- create mode 100755 0x01-variables_if_else_while/3-print_alphabets.c
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(c);
+	for (c = 'A'; c <= 'Z'; c++)
+		putchar(c);
+	putchar('\n');
+	return (0);
+}
