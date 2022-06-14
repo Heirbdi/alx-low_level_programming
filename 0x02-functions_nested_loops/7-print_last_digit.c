f14047b376357146fb367e557dd568aecd1babde
Heirbdi <heritierzlatan2@gmail.com>
2022-06-13 21:55:12 -0700

    Last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,11 +9,10 @@ int print_last_digit(int n)
 	int a;
 
 	if (n < 0)
-		a = (n % 10) * -1;
-	else
-	{
-		a = n % 10;
-	}
+		n = -n;
+	a = n % 10;
+	if (a < 0)
+		a = -a;
 	_putchar(a + '0');
 	return (a);
 }
