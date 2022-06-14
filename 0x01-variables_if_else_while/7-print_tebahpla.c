af9ec805507363a53b91aa8685228c506012bab9
Heirbdi <heritierzlatan2@gmail.com>
2022-06-13 21:19:48 -0700

    Printlowcase

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,3 +1,17 @@
-[master 36779bc] Lowcase alphabet in reverse
- 1 file changed, 16 insertions(+)
- create mode 100755 0x01-variables_if_else_while/7-print_tebahpla.c
+#include <stdio.h>
+/**
+ * main -Entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char alphabet;
+
+	for (alphabet = 'z'; alphabet >= 'a'; alphabet--)
+	{
+		putchar(alphabet);
+	}
+	putchar('\n');
+	return (0);
+}
