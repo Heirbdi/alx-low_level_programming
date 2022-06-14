91fb759688fbc65944660d1b00cc9cda1469fc75
Heirbdi <heritierzlatan2@gmail.com>
2022-06-13 21:40:33 -0700

    All possible combinations in two two-digit

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,3 +1,31 @@
-[master 02b6ef6] All possible combination of 2 two-digit numbers
- 1 file changed, 32 insertions(+)
- create mode 100755 0x01-variables_if_else_while/102-print_comb5.c
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0
+ */
+int main(void)
+{
+	int a, b;
+
+	for (a = 0; a < 100; a++)
+	{
+		for (b = 1; b < 100; b++)
+		{
+			if (a < b)
+			{
+				putchar((a / 10) + '0');
+				putchar((a % 10) + '0');
+				putchar(' ');
+				putchar((b / 10) + '0');
+				putchar((b % 10) + '0');
+				if (!(a == 98 && b == 99))
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
