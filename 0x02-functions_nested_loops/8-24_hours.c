33d1f2caa346a0b250c8e8cefe4a8155bdd17cfa
Heirbdi <heritierzlatan2@gmail.com>
2022-06-13 22:07:00 -0700

    Print every minute of the day of jack Bauer

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,39 +1,26 @@
-On branch master
-Your branch is up to date with 'origin/master'.
+#include "main.h"
+/**
+ * jack_bauer - prints every minute of the day of Jack Bauer
+ * starting from 00:00 to 23:59
+ */
+void jack_bauer(void)
+{
+	int i, j;
 
-Changes not staged for commit:
-  (use "git add/rm <file>..." to update what will be committed)
-  (use "git restore <file>..." to discard changes in working directory)
-	deleted:    ../0x00-hello_world/.3-name.swp
-	deleted:    ../0x00-hello_world/.5-printf.c.swp
-	modified:   ../0x00-hello_world/1-compiler
-	modified:   ../0x00-hello_world/100-intel
-	modified:   ../0x00-hello_world/101-quote.c
-	modified:   ../0x00-hello_world/2-assembler
-	modified:   ../0x00-hello_world/3-name
-	modified:   ../0x00-hello_world/4-puts.c
-	modified:   ../0x00-hello_world/5-printf.c
-	modified:   ../0x00-hello_world/6-size.c
-	modified:   ../0x01-variables_if_else_while/0-positive_or_negative.c
-	modified:   ../0x01-variables_if_else_while/1-last_digit.c
-	modified:   ../0x01-variables_if_else_while/101-print_comb4.c
-	modified:   ../0x01-variables_if_else_while/102-print_comb5.c
-	modified:   ../0x01-variables_if_else_while/2-print_alphabet.c
-	modified:   ../0x01-variables_if_else_while/3-print_alphabets.c
-	modified:   ../0x01-variables_if_else_while/4-print_alphabt.c
-	modified:   ../0x01-variables_if_else_while/5-print_numbers.c
-	modified:   ../0x01-variables_if_else_while/6-print_numberz.c
-	modified:   ../0x01-variables_if_else_while/7-print_tebahpla.c
-	modified:   ../0x01-variables_if_else_while/8-print_base16.c
-	modified:   ../0x01-variables_if_else_while/9-print_comb.c
-	modified:   7-print_last_digit.c
-	modified:   ../0x05-pointers_arrays_strings/100-atoi.c
-	modified:   ../0x05-pointers_arrays_strings/101-keygen.c
-	modified:   ../0x05-pointers_arrays_strings/8-print_array.c
-	modified:   ../0x05-pointers_arrays_strings/9-strcpy.c
-
-Untracked files:
-  (use "git add <file>..." to include in what will be committed)
-	8-24_hours.c
-
-no changes added to commit (use "git add" and/or "git commit -a")
+	i = 0;
+	while (i < 24)
+	{
+		j = 0;
+		while (j < 60)
+		{
+			_putchar((i / 10) + '0');
+			_putchar((i % 10) + '0');
+			_putchar(':');
+			_putchar((j / 10) + '0');
+			_putchar((j % 10) + '0');
+			_putchar('\n');
+			j++;
+		}
+		i++;
+	}
+}
