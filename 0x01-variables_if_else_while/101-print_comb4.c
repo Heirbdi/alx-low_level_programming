d74335907757ad02dc2ebfb71702cadc925d6680
Heirbdi <heritierzlatan2@gmail.com>
2022-06-13 21:35:47 -0700

    All possible combinations

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,3 +1,32 @@
-[master 2e56ca7] Different combinations of three digits
- 1 file changed, 36 insertions(+)
- create mode 100755 0x01-variables_if_else_while/101-print_comb4.c
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: (0) - success
+ */
+int main(void)
+{
+	int i, j, z;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			for (z = 0; z <= 9; z++)
+			{
+				if (i != j && j != z && i != z && i < j && z > j)
+				{
+					putchar(i + '0');
+					putchar(j + '0');
+					putchar(z + '0');
+					if (!(i == 7 && j == 8 && z == 9))
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
