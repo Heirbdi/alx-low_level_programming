6b1bd585204582301620a8492e8e84b27242a434
Heirbdi <heritierzlatan2@gmail.com>
2022-06-13 21:24:31 -0700

    The number of base 16 in lowercase

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,3 +1,22 @@
-[master 3a6eb02] Base 16 in lowcase
- 1 file changed, 26 insertions(+)
- create mode 100755 0x01-variables_if_else_while/8-print_base16.c
+#include <stdio.h>
+/**
+ * main - return the results of thr function
+ * Description: print the hexadecimal numbers
+ * Return: 0
+ */
+int main(void)
+{
+	char alphabet;
+	int num;
+
+	for (num = 0; num < 10; num++)
+	{
+		putchar(num + '0');
+	}
+	for (alphabet = 'a'; alphabet <= 'f'; alphabet++)
+	{
+		putchar(alphabet);
+	}
+	putchar('\n');
+	return (0);
+}
