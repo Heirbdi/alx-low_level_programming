8d7b941c927dd4647f4efe2d63b4a236e77edaa6
Heirbdi <heritierzlatan2@gmail.com>
2022-07-14 10:55:38 -0700

    Instructions

--- 0x12-singly_linked_lists/main/4-main.c
+++ 0x12-singly_linked_lists/main/4-main.c
@@ -1,25 +1,3 @@
-#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
-#include "lists.h"
-
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    list_t *head;
-
-    head = NULL;
-    add_node_end(&head, "Bob");
-    add_node_end(&head, "&");
-    add_node_end(&head, "Kris");
-    add_node_end(&head, "love");
-    add_node_end(&head, "asm");
-    print_list(head);
-    free_list(head);
-    head = NULL;
-    return (0);
-}
+[master e0f44bd] The node lists
+ 1 file changed, 25 insertions(+)
+ create mode 100644 0x12-singly_linked_lists/main/4-main.c
