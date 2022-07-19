8850d7770b2a80ab42cdd9586775caa43bed87cb
Heirbdi <heritierzlatan2@gmail.com>
2022-07-19 13:11:28 -0700

    A function that print all element

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,18 +1,24 @@
-#include <stdio.h>
-#include "lists.h"
-/**
- * print_listint - prints all elements of a list_t list
- * @h: singly linked list print
- * Return: the number of nodes
- */
-size_t print_listint(const listint_t *h)
-{
-	size_t i;
+On branch master
+Your branch is up to date with 'origin/master'.
 
-	for (i = 0; h; i++)
-	{
-		printf("%d\n", h->n);
-		h = h->next;
-	}
-	return (i);
-}
+Changes not staged for commit:
+  (use "git add <file>..." to update what will be committed)
+  (use "git restore <file>..." to discard changes in working directory)
+	modified:   0-print_listint.c
+	modified:   1-listint_len.c
+	modified:   10-delete_nodeint.c
+	modified:   2-add_nodeint.c
+	modified:   3-add_nodeint_end.c
+	modified:   4-free_listint.c
+	modified:   5-free_listint2.c
+	modified:   6-pop_listint.c
+	modified:   7-get_nodeint.c
+	modified:   8-sum_listint.c
+	modified:   9-insert_nodeint.c
+	modified:   main/0-main.c
+
+Untracked files:
+  (use "git add <file>..." to include in what will be committed)
+	.2-add_nodeint.c.swp
+
+no changes added to commit (use "git add" and/or "git commit -a")
